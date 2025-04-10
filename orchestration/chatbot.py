aa4353849e7de4a767565ae9eeeaddc6625b656d
calso-christian <calso.cpr@gmail.com>
2025-04-10 15:41:33 +0800

    Added streaming functionality

--- orchestration/chatbot.py
+++ orchestration/chatbot.py
@@ -1,5 +1,5 @@
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, trim_messages
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 from langgraph.checkpoint.memory import MemorySaver
@@ -22,15 +22,38 @@ prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
+trimmer = trim_messages(
+    max_tokens=65,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
 
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
 
 # Define a function that calls the model given a MessagesState
 def call_model(state: MessagesState):
+    trimmed_messages = trimmer.invoke(state["messages"])
     prompt = prompt_template.invoke(state)
-    response = model.invoke(prompt)
-    return {"messages":response}
+
+    print("\nMisha is typing...")
+
+    stream=model.stream(prompt)
+
+    full_text=""
+    
+    for chunk in stream:
+        content = chunk.content if hasattr(chunk, "content") else chunk.delta
+        if content:
+            print(content, end="", flush=True)
+            full_text += content
+    
+    print()
+    # response = model.invoke(prompt)
+    return {"messages":AIMessage(content=full_text)}
 
 
 # Define the (single) node in the graph
@@ -55,7 +78,6 @@ def chatbot():
         input_message=[HumanMessage(query)]
 
         output = app.invoke({"messages":input_message}, config)
-        output['messages'][-1].pretty_print()
 
 
 if __name__ == "__main__":
