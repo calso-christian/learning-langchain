7474457fb934da6073bae43db07981da5368773a
calso-christian <calso.cpr@gmail.com>
2025-04-10 08:08:44 +0800

    Started extraction example and Person Class

---
+++ examples/extraction.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+class Person(BaseModel):
+    name: Optional[str] = Field(default=None, description="The name of the person from the text")
+    hair_color: Optional[str] = Field(default=None, description="The color of the hair of the person if stated")
+    height_in_meters: Optional[str] = Field(default=None, description="Height of the person in Meters")
+
+
+
